103961402374f6b78670ba42549c2a19322385ce
Lorena-u <lorena.agaj@student.uni-pr.edu>
2025-02-10 20:24:15 +0100

    Krijimi i nje loje ne C++
    Shkrimi i pjeses fillestare te  nje kodi ne C++ me ane te komandave te nevojshme.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include<cstdlib>
+#include<ctime>
 using namespace std;
+
+//Deklarimi i funksioneve
+void printGreeting();
+int generateRandomNumber(int lower, int upper);
+void startGame();
+bool isGuessCorrect(int guess,int secretNumber);
+
 int main() {
+    //Krijo nje numer te rastesishem per loje
+    int secretNumber=generateRandomNumber(1,100);
 
-    cout << "Hello World!" << endl;
+    // Kjo pjese i tregon programit te shfaq  mesazhin
+    printGreeting();
+    // Kjo pjese i tregon programit te filloj lojen
+    startGame();
     return 0;
-    
-
 }
